9666fef829ab2aeb8dfed9a22f7ab5174fada76d
Josica Ganesamurthy <josica.ganesamurthy@gmail.com>
2017-01-12 14:29:46 -0500

    Update 20170112115801_create_votes.rb

--- db/migrate/20170112115801_create_votes.rb
+++ db/migrate/20170112115801_create_votes.rb
@@ -1,8 +1,8 @@
 class CreateVotes < ActiveRecord::Migration
   def change
     create_table :votes do |t|
-      t.interger :up_or_down, null: false
-      t.interger :votable_id, null: false
+      t.integer :up_or_down, null: false
+      t.integer :voteable_id, null: false
       t.string :voteable_type, null: false
     end
   end
