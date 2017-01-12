e360ecc13d337748659be20559003f45725e617b
Josica Ganesamurthy & Sam Glickman <pair+josica.sglickman17@devbootcamp.com>
2017-01-12 14:50:04 -0500

    Fix syntax errors

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,7 @@
 class Question < ActiveRecord::Base
   # Remember to create a migration!
+  validates :title, :body_question, :total_votes, presence:  true
+  belongs_to :user
+  has_many :votes, as: :voteable
+  has_many :comments, as: :commentable
 end
