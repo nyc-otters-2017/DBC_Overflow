52376e8e76a25353933cdc452983470075a7b1c0
Jeremy J Badillo <email": null>
2017-01-12 14:31:18 -0500

    complete questions migration class

--- db/migrate/20170112115725_create_questions.rb
+++ db/migrate/20170112115725_create_questions.rb
@@ -1,4 +1,12 @@
 class CreateQuestions < ActiveRecord::Migration
   def change
+    create_table :questions do |t|
+      t.integer :user_id, null: false
+      t.string :title, null: false
+      t.string :body_question, null: false
+      t.intger :total_votes, null: false
+
+      t.timestamps(null: false)
+    end
   end
 end
