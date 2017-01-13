a79ac9bf06392d6823a1cbf11e68bcbea91fd27e
Josica <josica.ganesamurthy@gmail.com>
2017-01-12 21:18:51 -0500

    Fix create new user route

--- app/controllers/question.rb
+++ app/controllers/question.rb
@@ -18,6 +18,18 @@ get '/questions/:id' do
   @question.answers.each do |answer|
     answer.get_total_votes
   end
-  
+
   erb :'questions/show'
+end
+
+post '/questions' do
+  require_user
+  @q = Question.new(params[:question])
+  # binding.pry
+  if @q.save
+    redirect "/questions/#{@q.id}"
+  else
+    @errors = @q.errors.full_messages
+    erb:'/questions/new'
+  end
 end
